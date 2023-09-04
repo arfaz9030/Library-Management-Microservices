79fbb46e49878ea0e92bfa3ae5565a138941d424
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2023-09-04 07:07:10 +0530

    2nd commit 2nd microservices

---
+++ library-management-book-microservice/src/main/java/com/tcs/library/librarymanagementbookmicroservice/LibraryManagementBookMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.library.librarymanagementbookmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementBookMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementBookMicroserviceApplication.class, args);
+	}
+
+}
---
+++ library-management-book-microservice/src/test/java/com/tcs/library/librarymanagementbookmicroservice/LibraryManagementBookMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.library.librarymanagementbookmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementBookMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
