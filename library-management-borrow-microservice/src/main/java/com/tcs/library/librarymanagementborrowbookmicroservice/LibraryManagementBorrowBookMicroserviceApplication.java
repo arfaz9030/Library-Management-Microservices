3a127a66f956f27f9030c89b1c97ca0e4ab81619
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2023-09-04 09:15:44 +0530

    library borrow book microservice

---
+++ library-management-borrow-microservice/src/main/java/com/tcs/library/librarymanagementborrowbookmicroservice/LibraryManagementBorrowBookMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.library.librarymanagementborrowbookmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementBorrowBookMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementBorrowBookMicroserviceApplication.class, args);
+	}
+
+}
---
+++ library-management-borrow-microservice/src/test/java/com/tcs/library/librarymanagementborrowbookmicroservice/LibraryManagementBorrowBookMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.library.librarymanagementborrowbookmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementBorrowBookMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
